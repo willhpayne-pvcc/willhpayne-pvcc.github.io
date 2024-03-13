7cd9edac289e640a37e69fdce631f5851a339f9c
William Payne <160060227+willhpayne-pvcc@users.noreply.github.com>
2024-03-13 14:23:38 -0400

    Add files via upload

---
+++ python/helloworld.py
@@ -0,0 +1,11 @@
+'''
+Name: William Payne 
+Prog Purpose: Hello World
+
+'''
+
+print("Hello World! This is my first Python program.")
+print("I can program in HTML, which is awesome!")
+print("However, HTML cannot do arithmetic....")
+print("But Python can do \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("and lots more!!!")
