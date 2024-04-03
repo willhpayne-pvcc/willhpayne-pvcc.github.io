5eb1e04b731787e9533b86f742af2c038865d3fc
William Payne <160060227+willhpayne-pvcc@users.noreply.github.com>
2024-04-03 13:58:28 -0400

    Add files via upload

--- python/pizza.py
+++ python/pizza.py
@@ -1,83 +1,97 @@
-# Name: William Payne, Tanner Jenkins
-# Purpose: Pizza calc
-
-import datetime 
-
-# Define global variables
-SALES_TAX_RATE = 0.055
-PIZZA_PRICES = {'s': 9.99, 'm': 12.99, 'l': 14.99, 'xl': 17.99}
-DRINK_PRICE = 3.99
-BREADSTICK_PRICE = 6.99
-
-# Initialize order counts
-order_counts = {'s': 0, 'm': 0, 'l': 0, 'xl': 0}
-num_drinks = 0
-num_breadsticks = 0
-num_pizzas = 0  # Add this line to initialize the number of pizzas
-
-def main():
-    global order_counts, num_drinks, num_breadsticks, num_pizzas
-    another_order = True
-    display_menu()
-    
-    while another_order:
-        get_user_data()
-        perform_calculations()
-        display_results()
-        response = input("Would you like to place another order? (y/n): ")
-        if response.lower() != 'y':
-            break
-
-def get_user_data():
-    global order_counts, num_drinks, num_breadsticks, num_pizzas, size
-    size = input("Enter pizza size (S, M, L, XL): ").lower()
-    if size in order_counts:
-        order_counts[size] += 1
-    else:
-        print("Invalid pizza size entered.")
-    
-    num_pizzas += int(input("How many pizzas do you want? "))  # Ask for the number of pizzas
-    num_drinks += int(input("How many drinks do you want? "))
-    num_breadsticks += int(input("How many breadsticks do you want? "))
-
-def perform_calculations():
-    global subtotal, sales_tax, total, order_counts, num_drinks, num_breadsticks, num_pizzas, subtotal_pizza
-    subtotal_pizza = sum(order_counts[size] * PIZZA_PRICES[size] for size in order_counts)
-    subtotal = sum(order_counts[size] * PIZZA_PRICES[size] for size in order_counts)
-    subtotal += num_drinks * DRINK_PRICE + num_breadsticks * BREADSTICK_PRICE
-    sales_tax = subtotal * SALES_TAX_RATE
-    total = subtotal + sales_tax
-
-def display_results():
-    print('------------------------------')
-    print('****** Gaias Pizzeria ******')
-    print('Your delicious local pizza place')
-    print('-------------------------------')
-    print(f"{size.upper()} Pizza   \t$ {subtotal_pizza}")
-    print('Drinks       \t$', num_drinks * DRINK_PRICE)
-    print('Breadsticks  \t$', num_breadsticks * BREADSTICK_PRICE)
-    print('------------------------------')
-    print(f'Subtotal     \t${subtotal:,.2f}')
-    print(f'Sales Tax    \t${sales_tax:,.2f}')
-    print(f'Total        \t${total:,.2f}')
-    print('-------------------------------')
-    current_time = datetime.datetime.now()
-    formatted_time = current_time.strftime("%Y-%m-%d %I:%M %p")
-    print(formatted_time)
-
-def display_menu():
-    print('''
-    ****** Welcome Gaias Pizzeria ******
-    ******        Menu            ******
-    s: Small \t$9.99
-    m: Medium\t$12.99
-    l: Large \t$14.99
-    xl: Extra-Large \t$17.99
-
-    Drink: \t$3.99
-    Breadsticks: \t$6.99
-    Sales Tax:\t5.5%
-    ''')
-
-if __name__ == "__main__":
-    main()
+# Tanner & Will 
+# Prog Purpose: This program finds the cost of a pizza meal
+#   This program uses TUPLES for the pizza sizes and prices
+
+import datetime
+
+######################## define items and prices in tupeles and tax rate ###########################
+
+#indexes--------> 0          1         2          3
+PIZZA_SIZE =     'S',       'M',      "L",       'X',
+PIZZA_PRICE =    9.99,      12.99,   17.99,      21.99,
+
+DRINK_PRICE = 3.99
+STICKS_PRICE = 6.99
+
+SALES_TAX_RATE = .055
+
+##################### define globals #############################
+
+num_pizzas = 0
+num_drinks = 0
+num_breadsticks = 0
+
+cost_pizza = 0
+cost_drinks = 0
+cost_breadsticks = 0
+
+subtotal = 0
+taxamt = 0
+total = 0
+
+####################### define prog functions ##########################
+
+def main():
+
+    more = True
+
+    while more:
+        get_user_data()
+        perform_calculations()
+        display_results()
+
+        askAgain = input('Would you like to order again (Y or N)?: ')
+        if askAgain.upper() == "N" or askAgain == "n":
+            more = False
+            print("Thank you for your order. Emjoy your meal!")
+
+def get_user_data():
+    global type_pizza, num_pizzas, num_drinks, num_breadsticks
+
+    part1 = "Size of pizza you would like to order: "
+    part2 = "\n\tS for Small \n\tM for Medium \n\tL for Large \n\tX for Extra Large \nSize: "
+    type_pizza = input(part1 + part2)
+    type_pizza = type_pizza.upper()
+
+    num_pizzas = int(input("How many pizzas would you like to order? "))
+    num_drinks = int(input("Number of drinks: "))
+    num_breadsticks = int(input("Number of breadsticks: "))
+
+
+def perform_calculations():
+    global size, cost_pizza, cost_drinks, cost_breadsticks, taxamt, total, subtotal
+
+    for i in range(len(PIZZA_SIZE)):
+        if type_pizza == PIZZA_PRICE[i]:
+            cost_pizza = num_pizzas * PIZZA_PRICE[i]
+
+    cost_drinks = num_drinks * DRINK_PRICE
+    cost_breadsticks = num_breadsticks * STICKS_PRICE
+
+    subtotal = cost_pizza + cost_drinks + cost_breadsticks
+    taxamt = subtotal * SALES_TAX_RATE
+    total = subtotal + taxamt
+
+def display_results():
+    currency = '8,.2f'
+    dt_full = str(datetime.datetime.now())
+    dt_short = dt_full[1:16]
+    line =('-------------------------------------')
+
+    print (line)
+    print ('*********** PALOMA PIZZA ************')
+    print ('**** Your neighborhood pizzaria! ****')
+    print (               dt_short)
+    print (line)
+    print ("Number of pizzas: " + str(num_pizzas))
+    print (line)
+    print ("Pizzas:          \t $" + format(cost_pizza, currency))
+    print ("Drinks:          \t $" + format(cost_drinks, currency))
+    print ("Bread Sticks:    \t $" + format(cost_breadsticks, currency))
+    print (line)
+    print ("Subtotal:        \t $" + format(subtotal, currency))
+    print ("Tax:             \t $" + format(taxamt, currency))
+    print ("Total:           \t $" + format(total, currency))
+    print (line)
+
+main()
